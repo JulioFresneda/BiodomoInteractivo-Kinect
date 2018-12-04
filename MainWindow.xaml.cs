460e43d03f657537c89963a3057a6fb9da54019c
juliofgx17 <juliofgx17@gmail.com>
2018-12-04 21:07:27 +0100

    menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
     using System.Windows.Controls;
     using Microsoft.Kinect;
     using Microsoft.Kinect.Wpf.Controls;
-    using Microsoft.Samples.Kinect.ControlsBasics.DataModel;
+
 
     /// <summary>
     /// Interaction logic for MainWindow
@@ -34,8 +34,8 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             this.kinectRegion.KinectSensor = KinectSensor.GetDefault();
 
             //// Add in display content
-            var sampleDataSource = SampleDataSource.GetGroup("Group-1");
-            this.itemsControl.ItemsSource = sampleDataSource;
+            //var sampleDataSource = SampleDataSource.GetGroup("Group-1");
+            
         }
 
         /// <summary>
@@ -44,32 +44,14 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
         /// <param name="sender">Event sender</param>
         /// <param name="e">Event arguments</param>
         private void ButtonClick(object sender, RoutedEventArgs e)
-        {            
-            var button = (Button)e.OriginalSource;
-            SampleDataItem sampleDataItem = button.DataContext as SampleDataItem;
+        {
 
-            if (sampleDataItem != null && sampleDataItem.NavigationPage != null)
-            {
-                backButton.Visibility = System.Windows.Visibility.Visible;
-                navigationRegion.Content = Activator.CreateInstance(sampleDataItem.NavigationPage);
-            }
-            else
-            {
-                var selectionDisplay = new SelectionDisplay(button.Content as string);
-                this.kinectRegionGrid.Children.Add(selectionDisplay);
-
-                // Selection dialog covers the entire interact-able area, so the current press interaction
-                // should be completed. Otherwise hover capture will allow the button to be clicked again within
-                // the same interaction (even whilst no longer visible).
-                selectionDisplay.Focus();
-
-                // Since the selection dialog covers the entire interact-able area, we should also complete
-                // the current interaction of all other pointers.  This prevents other users interacting with elements
-                // that are no longer visible.
-                this.kinectRegion.InputPointerManager.CompleteGestures();
-
-                e.Handled = true;
-            }
+            ScrollViewerSample OP = new ScrollViewerSample();
+            var host = new Window();
+            host.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            host.SourceInitialized += (s, a) => host.WindowState = WindowState.Maximized;
+            host.Content = OP;
+            host.Show();
         }
 
         /// <summary>
